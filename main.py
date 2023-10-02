89179964fea144b0fb434362bf143ac41c5e424d
d-rooX <bot0463@gmail.com>
2023-10-02 20:00:40 +0300

    created base architecture for app

---
+++ main.py
@@ -0,0 +1,18 @@
+import module_tools
+
+module_tools.say_hello()
+likes_count = 0
+
+
+def add_like():
+    global likes_count
+    likes_count += 1
+
+# Repository
+#   -   main.py
+#   -   module_tools.py
+#
+#
+#
+
+
