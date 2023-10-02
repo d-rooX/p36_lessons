086db3ceaaf75c72e3adc5ee6b593fd4ad1b4101
d-rooX <bot0463@gmail.com>
2023-10-02 21:11:14 +0300

    implemented loading comments from file

--- comments/comments_function.py
+++ comments/comments_function.py
@@ -1,4 +1,20 @@
+import os.path
+
 comments = []
+
+def load_comments_from_file():
+    global comments
+
+    if not os.path.exists('./comments.db'):
+        # creating file
+        with open('./comments.db', 'w'):
+            pass
+
+    with open('./comments.db', 'r') as file:
+        for line in file.readlines():
+            comments.append(line)
+
+
 
 def add_comment(text):
     global comments
