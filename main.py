78219ee921b42eae99b058341cc895d0501b13e2
d-rooX <bot0463@gmail.com>
2023-10-02 20:20:18 +0300

    added more comments

--- main.py
+++ main.py
@@ -14,5 +14,18 @@ def add_like():
 #
 #
 #
-
-
+#
+#   створити репозиторій
+#          BRANCH NAME
+# git init -b main
+#
+#   додати файли до репозиторію
+#      <filename or directory name>
+# git add .
+#
+#   створити комміт
+# git commit -m "message"
+#
+#   видалити файли з репозиторію (з відстеження)
+# git rm --cached
+#
