86fe084f0b884a2c837562ac0bee0e343cf736af
d-rooX <bot0463@gmail.com>
2023-10-02 21:20:31 +0300

    loading comments on enter app, saving on close

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ from likes import likes_function
 
 module_tools.say_hello()
 
+comments_function.load_comments_from_file()
+
 while True:
-    action = input('Write your action: [add_like, add_comment]: ')
+    action = input('Write your action: [add_like, add_comment, quit]: ')
     match action:
         case 'add_like':
             likes_function.add_like()
         case 'add_comment':
             text = input('Write comment text: ')
             comments_function.add_comment(text)
+        case 'quit':
+            break
+
+comments_function.save_comments_to_file()
 
 
 # Repository
